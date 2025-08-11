751a014039f5fc028f1c51dfee522c41c4f78a59
jabba2324 <until.acetic_0o@icloud.com>
2025-08-11 18:18:43 +0100

    Update jsondiff.rb
    use correct url

--- Formula/jsondiff.rb
+++ Formula/jsondiff.rb
@@ -1,7 +1,7 @@
 class Jsondiff < Formula
   desc "Command-line tool to compare two JSON files and show their differences"
-  homepage "https://github.com/chrissewell/jsondiff"
-  url "https://github.com/chrissewell/jsondiff/archive/v1.0.0.tar.gz"
+  homepage "https://github.com/jabba2324/jsondiff"
+  url "https://github.com/jabba2324/jsondiff/archive/v1.0.0.tar.gz"
   sha256 "REPLACE_WITH_ACTUAL_SHA256"
   license "MIT"
 
