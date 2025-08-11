388f92e9b8dec2161bd83d5dd619d0fa6ad50c86
Chris <until.acetic_0o@icloud.com>
2025-08-11 18:26:05 +0100

    update sha

--- Formula/jsondiff.rb
+++ Formula/jsondiff.rb
@@ -2,7 +2,7 @@ class Jsondiff < Formula
   desc "Command-line tool to compare two JSON files and show their differences"
   homepage "https://github.com/chrissewell/jsondiff"
   url "https://github.com/chrissewell/jsondiff/archive/v1.0.0.tar.gz"
-  sha256 "REPLACE_WITH_ACTUAL_SHA256"
+  sha256 "5bbcf62751a17cc6ce3690bf77a9fb23336cbc5c72e3b6a601ffc99f334276bd"
   license "MIT"
 
   depends_on "go" => :build
